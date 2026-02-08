b0c3b69030485b8b94658cea240f82af7a9d82df
joshguti04 <135501730+joshg04@users.noreply.github.com>
2026-02-07 18:37:07 -0600

    added pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Hero from './pages/Hero';
 import About from './pages/About';
 import Products from './pages/Explore';
